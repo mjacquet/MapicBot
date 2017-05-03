69739d8ab10893fc9d6b498c9e4e920fc5f5de98
Maxime Jacquet <mjacquet@salesforce.com>
2017-05-03 19:10:28 +0200

    get vision service token

--- modules/vision-service-mock.js
+++ modules/vision-service-mock.js
@@ -1,7 +1,55 @@
 "use strict";
 
 exports.classify = imageURL => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("X-WING");
-    }, 2000);
+
+  var jwt = require('jsonwebtoken');
+  var request = require('request');
+
+  var url = process.env.EINSTEIN_VISION_URL
+  var private_key = process.env.EINSTEIN_VISION_PRIVATE_KEY
+  var account_id = process.env.EINSTEIN_VISION_ACCOUNT_ID
+
+  var reqUrl = `${url}v1/oauth2/token`;
+
+  // JWT payload
+  var rsa_payload = {
+    "sub":account_id,
+    "aud":url
+  }
+
+  var rsa_options = {
+    header:{
+      "alg":"RS256",
+      "typ":"JWT"
+    },
+    expiresIn: '1h'
+  }
+
+  // Sign the JWT payload
+  var assertion = jwt.sign(
+    rsa_payload,
+    private_key,
+    rsa_options
+  );
+
+  var options = {
+    url: reqUrl,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'accept': 'application/json'
+    },
+    body:`grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=${encodeURIComponent(assertion)}`
+  }
+
+  // Make the OAuth call to generate a token
+  request.post(options, function(error, response, body) {
+    var data = JSON.parse(body);
+    console.log(data["access_token"]);
+    resolve(data["access_token"]);
+  });
+
+  /*
+  setTimeout(() => {
+    resolve("X-WING");
+  }, 2000);*/
 });
