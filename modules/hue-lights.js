e4bd2aec24efe32a7519d4ce8795574671956b65
Maxime Jacquet <mjacquet@salesforce.com>
2017-05-05 14:12:55 +0200

    init hue and lights on

--- modules/hue-lights.js
+++ modules/hue-lights.js
@@ -2,20 +2,29 @@ const hbid = process.env.HUE_BRIDGE_ID;
 const hbtoken = process.env.HUE_BRIDGE_TOKEN;
 const rp = require('request-promise');
 var Episode7 = require('episode-7');
+var lights=["xwing":null,"tiefighter":null];
 
 function* hueLights(
-                       light){
+                       action,
+                       light=null){
 
+  if(lights['xwing']==null){
+    let hueinit = yield Episode7.call(
+      hueLightsInit
+    );
+    console.log('hue init');
+  }
 /*  var formData = {
     modelId: modelId,
     sampleLocation : resizedImgUrl
   }*/
   var options = {
-      url: `https://www.meethue.com/api/getbridge?token=${hbtoken}&bridgeid=${hbid}`,
-      method: 'GET',
+      url: `https://www.meethue.com/api/sendmessage?token=${hbtoken}`,
+      method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-      }
+      },
+      formData:{clipmessage:{bridgeid:hbid,clipcommand:{url:'/api/lights/1/state',method:'PUT',body:{"on":true}}}}
 
   }//formData:formData
   console.log('hueAPI request',options);
@@ -36,4 +45,39 @@ function* hueLights(
 
 }
 
+function* hueLightsInit(){
+
+/*  var formData = {
+    modelId: modelId,
+    sampleLocation : resizedImgUrl
+  }*/
+  var options = {
+      url: `https://www.meethue.com/api/getbridge?token=${hbtoken}&bridgeid=${hbid}`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+
+  }//formData:formData
+  console.log('hueAPI init request',options);
+  let { body, isUnauthorized } = yield Episode7.call((options) => {
+    return rp(options)
+      .then( body => ({ body }) )
+      .catch( error => {
+        if(error.statusCode === 401) {
+          console.log("------- HUE API UNAUTHORIZED------------\n-----UPDATE TOKEN------")
+        } else {
+          throw error;
+        }
+      })
+  },options);
+
+    let aLights=JSON.parse(body).lights;
+    console.log('Hue api init return lights:',aLights);
+
+    return aLights;
+
+}
+
 module.exports = hueLights;
+module.exports = hueLightsInit;
