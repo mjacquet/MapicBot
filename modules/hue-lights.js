e41c59f1e847bd4f441ef3e1b31f12e14148736e
Maxime Jacquet <mjacquet@salesforce.com>
2017-07-26 18:18:19 +0200

    Quick fix for cleaning episode7

--- modules/hue-lights.js
+++ modules/hue-lights.js
@@ -3,8 +3,6 @@
 const hbid = process.env.HUE_BRIDGE_ID;
 const hbtoken = process.env.HUE_BRIDGE_TOKEN;
 const rp = require('request-promise');
-let request = require('request');
-var Episode7 = require('episode-7');
 var lights={xwing:process.env.HUE_XWING_ID,tiefighter:process.env.HUE_TIEFIGHTER_ID,uwing:process.env.HUE_UWING_ID};
 var modes={orange:'{"on": true,"bri": 254,"hue": 6751,"sat": 254,"effect": "none","xy":[0.554,0.4116],"ct": 500,"alert": "lselect","colormode": "xy"}',
 green:'{"on": true,"bri": 254,"hue": 22463,"sat": 254,"effect": "none","xy": [0.2339,0.652],"ct": 153,"alert": "select","colormode": "xy"}',
