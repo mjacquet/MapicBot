e5f2f2052263b62bf4287d783a248db0463967cc
Maxime Jacquet <mjacquet@salesforce.com>
2017-11-02 22:25:15 +0100

    fix

--- server.js
+++ server.js
@@ -75,8 +75,6 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-app.get('/orderdone', handlers['orderdone']);
-
 app.post('/webhook', async(req, res) => {
   let events = req.body.entry[0].messaging;
   for (let i = 0; i < events.length; i++) {
